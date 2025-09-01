ddf5f1639c2807d25c1392f865c9c3343ce538ba
Lautaro Marguery <l.e.marguery@gmail.com>
2025-09-01 13:35:04 -0300

    Se creo al migracion para uqe no sea unico el dni

---
+++ migrations/Version20250822054825.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Remove UNIQUE constraint from national_id_number field in user table
+ */
+final class Version20250901120000 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Remove UNIQUE constraint from national_id_number field to allow duplicates';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE user DROP INDEX UNIQ_IDENTIFIER_NATIONAL_ID_NUMBER');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE user ADD UNIQUE KEY UNIQ_IDENTIFIER_NATIONAL_ID_NUMBER (national_id_number)');
+    }
+}
