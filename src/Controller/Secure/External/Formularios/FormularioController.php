e34967b245fd680fbecabe86dc0a923520c5b552
ARESLautaro95estudio <l.e.marguery@gmail.com>
2025-11-13 21:14:47 -0300

    Se comenzo con el desarrollo de formulario

---
+++ src/Controller/Secure/External/Formularios/FormularioController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Secure\External\Formularios;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+#[Route('secure/formularios-cliente')]
+final class FormularioController extends AbstractController
+{
+    #[Route('/', name: 'app_secure_external_formularios_servicios')]
+    public function index( Request $request): Response
+    {
+         return $this->render('secure/external/formularios_servicios/index.html.twig', [
+            'controller_name' => 'CuentasController',
+        ]);
+    }
+
+    #[Route('/', name: 'app_secure_external_formularios_servicios_carga')]
+    public function carga( Request $request): Response
+    {
+        $data = $request->request->all();
+
+        return $this->render('secure/external/formularios_servicios/index.html.twig', [
+            'controller_name' => 'FormularioController',
+        ]);
+    }
+}
