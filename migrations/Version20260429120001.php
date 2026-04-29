fbefad2e6d33db25b92482ecb5fefd3ebd463c73
apedrasa <alexpedrasa10@gmail.com>
2026-04-29 12:55:37 -0300

    fix

--- migrations/Version20260429120001.php
+++ migrations/Version20260429120001.php
@@ -11,14 +11,14 @@ final class Version20260429120001 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Agrega columnas articulo_codigo y comment a proyecto_items';
+        return 'Agrega columna articulo_codigo a proyecto_items';
     }
 
     public function up(Schema $schema): void
     {
         $this->addSql(<<<'SQL'
             ALTER TABLE proyecto_items
-                ADD articulo_codigo VARCHAR(50) DEFAULT NULL,
+                ADD articulo_codigo VARCHAR(50) DEFAULT NULL
         SQL);
     }
 
@@ -26,7 +26,7 @@ final class Version20260429120001 extends AbstractMigration
     {
         $this->addSql(<<<'SQL'
             ALTER TABLE proyecto_items
-                DROP COLUMN articulo_codigo,
+                DROP COLUMN articulo_codigo
         SQL);
     }
 }
